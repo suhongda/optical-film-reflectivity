6fc88334e0c154c0d0c6a1cc78244a12ea51a9fd
suhongda <59460073+suhongda@users.noreply.github.com>
2020-02-17 02:49:20 +0800

    Update 特征矩阵有gui.py

--- 特征矩阵有gui.py
+++ 特征矩阵有gui.py
@@ -1,10 +1,12 @@
 # -*-coding:utf-8-*-
 from tkinter import *
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
-from matplotlib.ticker import MultipleLocator,FormatStrFormatter
+from matplotlib.ticker import MultipleLocator
+import matplotlib.pyplot as plt
+import datetime
+
 
 class main:
 
@@ -16,31 +18,37 @@ class main:
         self.frm = Frame(self.root)      # 框架
         self.frm_top = Frame(self.frm)
         self.frm_mxcs = Frame(self.frm_top)
-        self.label_mxcs = Label(self.frm_mxcs,text='膜系层数:',width=10,height=2).grid(row=0, column=0)
+
+        Label(self.frm_mxcs, text='项目:', width=10, height=2,font=('微软雅黑',14),fg='red').grid(row=0, column=0)
+        self.var_mxcs11 = StringVar()  # 接收输入
+        self.entry_mxcs11 = Entry(self.frm_mxcs, textvariable=self.var_mxcs11, width=8,font=('微软雅黑',14),bd=0).grid(row=0, column=1)
+
+        self.label_mxcs = Label(self.frm_mxcs,text='膜系层数:',width=10).grid(row=1, column=0)
         self.var_mxcs = StringVar()  # 接收输入
-        self.entry_mxcs = Entry(self.frm_mxcs, textvariable=self.var_mxcs, width=5).grid(row=0, column=1)        # 输入膜层
-        self.button_mxcs = Button(self.frm_mxcs, text='确定', command=self.click).grid(row=0, column=2,padx=20)
-        self.frm_mxcs.grid(row=0, column=0)
+        self.entry_mxcs = Entry(self.frm_mxcs, textvariable=self.var_mxcs, width=13,bd=0).grid(row=1, column=1)        # 输入膜层
+        self.button_mxcs = Button(self.frm_mxcs, text='确定', command=self.click,width=6,
+                                                bd=0,bg='PaleGreen',activeforeground='yellow',activebackground='red').grid(row=1, column=2,padx=20)
+        self.frm_mxcs.grid(row=1, column=0)
         #入射角
         self.frm_rsj = Frame(self.frm_top)
-        self.label_rsj = Label(self.frm_rsj, text='入射角').grid(row=0, column=0, padx=5)
+        self.label_rsj = Label(self.frm_rsj, text='入射角').grid(row=1, column=0, padx=5)
         self.var_rsj = DoubleVar(value=0)  # 接收入射角输入
-        self.entry_rsj = Entry(self.frm_rsj, textvariable=self.var_rsj, width=5).grid(row=0, column=1)
-        self.frm_rsj.grid(row=0, column=1)
+        self.entry_rsj = Entry(self.frm_rsj, textvariable=self.var_rsj, width=5,bd=0).grid(row=1, column=1)
+        self.frm_rsj.grid(row=1, column=1)
         # 波长范围
         self.frm_wave_range = Frame(self.frm_top)
         self.var_max = DoubleVar(value=780)  # 上限
         self.var_min = DoubleVar(value=380)  # 下限
         self.var_spacing = DoubleVar(value=1)  # 波长间隔
-        self.label_wave_range = Label(self.frm_wave_range, text='波长范围:',width=15).grid(row=0, column=0)
-        self.label_max = Label(self.frm_wave_range, text='上限:').grid(row=0, column=3)
-        self.entry_range_max = Entry(self.frm_wave_range, textvariable=self.var_max, width=5).grid(row=0, column=4)
-        self.label_min = Label(self.frm_wave_range, text='下限:').grid(row=0, column=1)
-        self.entry_range_min = Entry(self.frm_wave_range, textvariable=self.var_min, width=5).grid(row=0, column=2)
-        self.label_spacing = Label(self.frm_wave_range, text='波长间隔:').grid(row=0, column=5)
-        self.entry_range_label_spacing = Entry(self.frm_wave_range, textvariable=self.var_spacing, width=5).grid(row=0, column=6)
-        self.frm_wave_range.grid(row=0, column=2, padx=20)
-        self.frm_top.grid(row=0, sticky='w')
+        self.label_wave_range = Label(self.frm_wave_range, text='波长范围:',width=15).grid(row=1, column=0)
+        self.label_max = Label(self.frm_wave_range, text='上限:').grid(row=1, column=3)
+        self.entry_range_max = Entry(self.frm_wave_range, textvariable=self.var_max, width=5, bd=0).grid(row=1, column=4)
+        self.label_min = Label(self.frm_wave_range, text='下限:').grid(row=1, column=1)
+        self.entry_range_min = Entry(self.frm_wave_range, textvariable=self.var_min, width=5, bd=0).grid(row=1, column=2)
+        self.label_spacing = Label(self.frm_wave_range, text='波长间隔:').grid(row=1, column=5)
+        self.entry_range_label_spacing = Entry(self.frm_wave_range, textvariable=self.var_spacing, width=5, bd=0).grid(row=1, column=6)
+        self.frm_wave_range.grid(row=1, column=2, padx=20)
+        self.frm_top.grid(row=1, sticky='w')
 
         # 底部
         self.frm_bottom = Frame(self.frm)
@@ -117,26 +125,26 @@ class main:
             if i == 0:
                 label_level_1 = Label(self.frm_characteristic_matrix, text='入射介质n0：').grid(row=i, column=0,pady=2)
                 self.n[i] = DoubleVar(value=1)
-                entry_rsjz = Entry(self.frm_characteristic_matrix, textvariable=self.n[0], width=8).grid(row=i, column=1)
+                entry_rsjz = Entry(self.frm_characteristic_matrix, textvariable=self.n[0], width=8,bd=0).grid(row=i, column=1)
             elif i == (mxcs_int+1):
                 label_level_1 = Label(self.frm_characteristic_matrix, text='衬底n' + str(mxcs_int + 1) + '：').grid( row=mxcs_int + 1, column=0)
                 self.n[i] = DoubleVar(value=1.52)
-                entry_cd = Entry(self.frm_characteristic_matrix, textvariable=self.n[i], width=8).grid(row=mxcs_int + 1, column=1)
-                button_confirm = Button(self.frm_characteristic_matrix, text='确定', command=self.confirm,width=8).grid(row=mxcs_int + 1, column=3)
+                entry_cd = Entry(self.frm_characteristic_matrix, textvariable=self.n[i], width=8,bd=0).grid(row=mxcs_int + 1, column=1)
+                button_confirm = Button(self.frm_characteristic_matrix, text='确定', command=self.confirm,width=8,
+                                                        bd=0,bg='PaleGreen',activeforeground='yellow',activebackground='red').grid(row=mxcs_int + 1, column=3)
             else:
                 Label(self.frm_characteristic_matrix, text='第' + str(i) + '层n' + str(i) + ':').grid(row=i, column=0)
-
                 self.n[i] = DoubleVar(value=1.45)
-                entry_n = Entry(self.frm_characteristic_matrix, textvariable=self.n[i], width=8).grid(row=i, column=1)
+                entry_n = Entry(self.frm_characteristic_matrix, textvariable=self.n[i], width=8,bd=0).grid(row=i, column=1)
                 Label(self.frm_characteristic_matrix,width=2).grid(row=i, column=2)
                 Label(self.frm_characteristic_matrix, text='厚度d' + str(i) + ':').grid(row=i, column=3)
                 self.d[i] = DoubleVar(value=100)
-                entry_d = Entry(self.frm_characteristic_matrix, textvariable=self.d[i], width=7).grid(row=i, column=4)
+                entry_d = Entry(self.frm_characteristic_matrix, textvariable=self.d[i], width=7,bd=0).grid(row=i, column=4)
                 Label(self.frm_characteristic_matrix, text='nm' ).grid(row=i, column=5)
 
     def draw_char(self, k, o, n, d, Wavelength):
         # 生成用于绘图的数据
-        reflectivity = []  # 能量反射率列表，空
+        self.reflectivity = []  # 能量反射率列表，空
         # 能量反射率计算
         for wavelength in Wavelength:
             cos_ar_k = self.cos_RefractionAngle_k(o, n)  # cosφk的计算
@@ -146,10 +154,10 @@ class main:
             BCp = self.CharacteristicMatrix(PYK, xwhd, k)  # p分量矩阵[BC]计算
             BCs = self.CharacteristicMatrix(SYK, xwhd, k)  # s分量矩阵[BC]计算
             R = self.reflex(BCp, BCs, PYK[0], SYK[0])  # 衬底和膜系组合的能量反射率
-            reflectivity.append(R * 100)
-        print('能量反射率：' + str(reflectivity))
+            self.reflectivity.append(R * 100)
+        print('能量反射率：' + str(self.reflectivity))
         self.subplot.cla()
-        self.subplot.plot(Wavelength, reflectivity, 'r')
+        self.subplot.plot(Wavelength, self.reflectivity, 'r',linewidth=0.5,alpha=1)
         self.subplot.spines['bottom'].set_linewidth(0.5)  # 设置底部坐标轴的粗细
         self.subplot.spines['left'].set_linewidth(0.5) # 设置左边坐标轴的粗细
         self.subplot.spines['right'].set_linewidth(0.5)  # 设置右边坐标轴的粗细
@@ -167,6 +175,8 @@ class main:
         self.canvas_chart.draw()
 
 
+
+
     def confirm(self):
         # 所有输入的数据都没有进行校验，当输入的数据错误是可能会出错
         k = int(self.var_mxcs.get())  # 膜系层数
@@ -176,11 +186,11 @@ class main:
         for i in range(1, k+2):
             n[i] = float(self.n[i].get())
 
-        d = [0] * (k+2)  # 初始化为k+1 大小，为了使 n，d 的下标对应
+        d1 = [0 for x in range(int(self.var_mxcs.get())+2)]   # 初始化为k+1 大小，为了使 n，d 的下标对应
         # d0 不知道存不存在，先设置为0吧
-        d[0] = 0
+        d1[0] = 0
         for i in range(1, k + 1):
-            d[i] = float(self.d[i].get())
+            d1[i] = float(self.d[i].get())
         # for i, j in self.n, self.d:
         #     if i.get() is None or j.get() is None:
         #         # 存在输入框为空，不能进行下去，不做过多处理
@@ -192,18 +202,57 @@ class main:
         Wavelength = self.wavelengthCaculate(wave_length_min, wave_length_max, wave_length_spacing)
         # 处理数据，没有对输入的数据进行验证，当输入的数据错误是可能会出错
         # 将处理好的数据当成参数，调用函数进行画图
-        self.draw_char(k, o, n, d, Wavelength)                            #################################################
-        print('膜系层数' + self.var_mxcs.get())
-        print('入射角' + str(self.var_rsj.get()))
-        print('上限' + str(self.var_max.get()))
-        print('下限' + str(self.var_min.get()))
-        print('波长间隔' + str(self.var_spacing.get()))
-        print('入射介质n0' + str(self.n[0]))
-        print('衬底' + str(self.n[k + 1].get()))
-        print('n:')
-        print(n)
-        print('d:')
-        print(d)
+        self.draw_char(k, o, n, d1, Wavelength) 
+        # 输出图片
+        fig = plt.figure()
+        m = fig.add_subplot(111)
+        plt.plot(Wavelength, self.reflectivity, 'r',linewidth=0.7,alpha=1)
+        m.spines['bottom'].set_linewidth(0.5)  # 设置底部坐标轴的粗细
+        m.spines['left'].set_linewidth(0.5) # 设置左边坐标轴的粗细
+        m.spines['right'].set_linewidth(0.5)  # 设置右边坐标轴的粗细
+        m.spines['top'].set_linewidth(0.5) # 设置上部坐标轴的粗细
+        m.xaxis.set_major_locator(MultipleLocator(60))
+        m.xaxis.set_minor_locator(MultipleLocator(20))  # 设置x轴次刻度间隔
+        m.yaxis.set_minor_locator(MultipleLocator(1))  # 设置y轴次刻度间隔
+        m.grid(linestyle='--',linewidth=0.4,which='major')        # 网格线
+        m.grid(linestyle='--', linewidth=0.4, which='minor')
+        m.tick_params(labelsize=7)#
+        labels = m.get_xticklabels() + m.get_yticklabels()#
+        [label.set_fontname('Times New Roman') for label in labels]#
+        m.set_xlabel('Wavelength (nm)', fontdict={'family' : 'Times New Roman','size': 8})  # 设置 x轴的名称
+        m.set_ylabel('Reflectivity (%)', fontdict={'family' : 'Times New Roman','size': 8})  # 设置 y轴的名
+        time1 = datetime.datetime.now().strftime('%m.%d-%H%M%S.%f')
+        plt.savefig(self.var_mxcs11.get()+'-'+time1+'.png', dpi=300)
+        # 输出txt文件
+        content1='项目:'+self.var_mxcs11.get()
+        content2='时间:'+datetime.datetime.now().strftime('%Y.%m.%d-%H:%M:%S.%f')
+        content3='层数:'+str(k)+'        '+'入射角:'+str(o)
+        content4=''
+        content5 = ''
+        for i in range(int(self.mxcs)+2):
+            if i==0  :
+                co1=str(i)+' '+str(n[i])+'   --'+'\n'
+                content4=content4+co1
+            elif i==int(self.mxcs)+1:
+                co1 = str(i) + ' ' + str(n[i]) + '   --' + '\n'
+                content4 = content4 + co1
+            else:
+                co1 = str(i) + ' ' + str(n[i]) +' '+str(d1[i])+ '\n'
+                content4 = content4 + co1
+        for i in range(int( (wave_length_max-wave_length_min)/wave_length_spacing)):
+            co2=str(Wavelength[i]) + ' ' + str(self.reflectivity[i])+ '\n'
+            content5 = content5 + co2
+        # 写入文件
+        with open(self.var_mxcs11.get()+'-'+time1 + '.txt', 'w') as f:
+            f.write(content1+'\n'
+                    +content2+'\n'
+                    +content3+'\n\n'
+                    +'层  折射率  厚度\n'
+                    +content4+'\n'
+                    +'波长  反射率\n'
+                    +content5)
+
+
 
     # 要计算波长范围输入
     def wavelengthCaculate(self, wmin, wmax, s):  # 要计算波长范围输入
